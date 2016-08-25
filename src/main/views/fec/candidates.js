0d6fd5774c968a58a8a39e519ef62f2bba740293
Brandon Kirby <brandon@escapepod.com>
2016-08-24 21:33:36 -0400

    Start showing the page of results we get back from querying the FEC API by candidate name.

--- src/main/views/fec/candidates.js
+++ src/main/views/fec/candidates.js
@@ -1,10 +1,49 @@
 import React from "react";
 
-const candidates = (props) => {
+const candidates = ({ candidates }) => {
+  const hasCandidates = candidates.length > 0;
+
+  if (hasCandidates) {
+    return showResults(candidates);
+  }
+
+  return emitNoResults();  
+};
+
+const showResults = (candidates) => {
   return (
-    <div>
-      <p>No results</p>
-    </div>
+    <ul>
+      { emitCandidates(candidates) }
+    </ul>
+  );
+};
+
+const emitCandidates = (candidates) => {
+  return candidates.map(({ id, name, office_sought }) => {
+    const office = decodeOffice(office_sought);
+
+    return (
+      <li key={ id }>{ name } -- { office } -- { id }</li>
+    );
+  });
+};
+
+const decodeOffice = (office_sought) => {
+  switch(office_sought) {
+  case "H":
+    return "House";
+  case "S":
+    return "Senate";
+  case "P":
+    return "President/Vice President";
+  default:
+    return "No idea.";
+  }
+};
+
+const emitNoResults = () => {
+  return (
+    <p>No results</p>
   );
 };
 
